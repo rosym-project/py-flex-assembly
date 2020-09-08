c2153713d7413c3d4692156fc2ef7649a6adfc45
christopher.kreis <ckreis@techfak.uni-bielefeld.de>
2020-09-08 14:04:31 +0200

    splits the table into multiple parts to prevent false collision with it's hull

--- gym_flexassembly/envs/flex_assembly_env.py
+++ gym_flexassembly/envs/flex_assembly_env.py
@@ -86,7 +86,7 @@ class FlexAssemblyEnv(EnvInterface):
         # self._p.loadURDF(os.path.join(self._urdfRoot_flexassembly, "objects/plane_solid.urdf"), useMaximalCoordinates=True) # Brauche ich fuer die hit rays
 
         # Table
-        table_id = self._p.loadURDF(os.path.join(self._urdfRoot_flexassembly+"/objects", "table_2.urdf"), useFixedBase=True, flags = self._p.URDF_USE_INERTIA_FROM_FILE)
+        table_id = self._p.loadURDF(os.path.join(self._urdfRoot_flexassembly+"/objects/table_2", "table_2.urdf"), useFixedBase=True, flags = self._p.URDF_USE_INERTIA_FROM_FILE)
         table_offset_world_x = 0.1
         table_offset_world_y = 1.2
         table_offset_world_z = 0
@@ -99,7 +99,7 @@ class FlexAssemblyEnv(EnvInterface):
         # Workpiece clamp 1
         workpiece_1_offset_table_x = -0.5
         workpiece_1_offset_table_y = -0.3
-        workpiece_1_offset_table_z = 1.3#0.707
+        workpiece_1_offset_table_z = 0.707
         workpiece_1_offset_world = [table_offset_world_x + workpiece_1_offset_table_x, table_offset_world_y + workpiece_1_offset_table_y, table_offset_world_z + workpiece_1_offset_table_z]
         # workpiece_1 = SpringClamp(pos=workpiece_1_offset_world, orn=[0,-0.131,0.991,0])workpiece_1 = SpringClamp(pos=workpiece_1_offset_world, orn=[0,-0.131,0.991,0])
         workpiece_1 = SpringClamp(pos=workpiece_1_offset_world)
@@ -107,14 +107,14 @@ class FlexAssemblyEnv(EnvInterface):
         # Workpiece clamp 2
         workpiece_2_offset_table_x = -0.5
         workpiece_2_offset_table_y = -0.5
-        workpiece_2_offset_table_z = 1.3#0.707
+        workpiece_2_offset_table_z = 0.707
         workpiece_2_offset_world = [table_offset_world_x + workpiece_2_offset_table_x, table_offset_world_y + workpiece_2_offset_table_y, table_offset_world_z + workpiece_2_offset_table_z]
         workpiece_2 = SpringClamp(pos=workpiece_2_offset_world)
 
         # Workpiece clamp 3
         workpiece_3_offset_table_x = -0.5
         workpiece_3_offset_table_y = -0.7
-        workpiece_3_offset_table_z = 1.3#0.707
+        workpiece_3_offset_table_z = 0.707
         workpiece_3_offset_world = [table_offset_world_x + workpiece_3_offset_table_x, table_offset_world_y + workpiece_3_offset_table_y, table_offset_world_z + workpiece_3_offset_table_z]
         workpiece_3 = SpringClamp(pos=workpiece_3_offset_world)
 
