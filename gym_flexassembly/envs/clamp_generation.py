6131ca077b4eeb12112150e6a37425dfde113bdf
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2020-09-25 15:43:06 +0200

    clamp generation now uses flex assembly env

--- gym_flexassembly/envs/clamp_generation.py
+++ gym_flexassembly/envs/clamp_generation.py
@@ -13,56 +13,45 @@ import pybullet_data
 import tqdm
 
 from gym_flexassembly import data as flexassembly_data
+from gym_flexassembly.envs.flex_assembly_env import FlexAssemblyEnv
 
 
-def generate_image(model_settings, camera_settings):
-    # connect to the physics simulation
-    physicsClient = p.connect(p.DIRECT)
-
-    # place the ground plane
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    planeId = p.loadURDF('plane.urdf')
-
+def generate_image(model_path, model_pose, camera_settings):
     # place a clamp
-    clamp_id = p.loadSDF(model_settings['path'], globalScaling=2.0)[0]
-    p.resetBasePositionAndOrientation(clamp_id, model_settings['pos'], model_settings['rot'])
-
-    # set up the camera
-    width = 1280
-    height = 720
-
-    view_matrix = p.computeViewMatrix(camera_settings['pos'],
-                                      camera_settings['target_pos'],
-                                      camera_settings['up'])
-
-    fov = 65
-    aspect = width / height
-    near = 0.16
-    far = 10
-    projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, near, far)
+    clamp_id = p.loadSDF(model_path)[0]
+    p.resetBasePositionAndOrientation(clamp_id, model_pose['pos'], model_pose['orn'])
 
     # take a picture
-    w, h, rgba, depth_buffer, segmentation = p.getCameraImage(width,
-                              height,
-                              view_matrix,
-                              projection_matrix,
+    w, h, rgba, depth_buffer, segmentation = p.getCameraImage(
+                              camera_settings['width'],
+                              camera_settings['height'],
+                              camera_settings['view_matrix'],
+                              camera_settings['projection_matrix'],
                               shadow=True,
                               renderer=p.ER_BULLET_HARDWARE_OPENGL)
 
-    # stop the physics simulation
-    p.disconnect()
+    # remove model
+    p.removeBody(clamp_id)
+
     return rgba[:, :, :3]
 
 
-def get_random_camera_settings(min_dist=0.2, max_dist=0.5, max_vert_up=0.3, max_ver_low=0.1):
-    pos_x = random.uniform(-max_vert_up, max_vert_up)
-    pos_y = random.uniform(-max_vert_up, max_vert_up)
-    pos_z = random.uniform(min_dist, max_dist)
+width = 1280
+height = 720
+fov = 65
+aspect = width / height
+near = 0.16
+far = 10
+projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, near, far)
+def get_random_camera_settings(target_pos, min_dist=0.2, max_dist=0.4, range_upper=0.3, range_lower=0.1):
+    pos_x = target_pos[0] + random.uniform(-range_upper, range_upper)
+    pos_y = target_pos[1] + random.uniform(-range_upper, range_upper)
+    pos_z = target_pos[2] + random.uniform(min_dist, max_dist)
     pos = [pos_x, pos_y, pos_z]
 
-    target_x = random.uniform(-max_ver_low, max_ver_low)
-    target_y = random.uniform(-max_ver_low, max_ver_low)
-    target_z = 0 
+    target_x = target_pos[0] + random.uniform(-range_lower, range_lower)
+    target_y = target_pos[1] + random.uniform(-range_lower, range_lower)
+    target_z = target_pos[2] 
     target_pos = [target_x, target_y, target_z]
 
     up_x = random.uniform(-1, 1)
@@ -70,15 +59,81 @@ def get_random_camera_settings(min_dist=0.2, max_dist=0.5, max_vert_up=0.3, max_
     up_z = random.uniform(-0.1, 0.1)
     up = [up_x, up_y, up_z]
 
+    view_matrix = p.computeViewMatrix(pos,
+                                      target_pos,
+                                      up)
     return {'pos': pos,
             'target_pos': target_pos,
-            'up': up}
+            'up': up,
+            'view_matrix': view_matrix,
+            'projection_matrix': projection_matrix,
+            'width': width,
+            'height': height}
 
 
-def get_model_settings(path, pos, rot):
-    return {'path': path,
-            'pos': pos,
-            'rot': rot}
+table_offset_x = 0.1
+table_offset_y = 1.2
+def generate_random_model_pose():
+    # orientation
+    # always rotate around z
+    # normal orn: [0, 0, x]
+    # normal height: 0.71
+
+    # low prob orn: [math.pi / 2, 0, x] or [3 * math.pi, 0, x]
+    # height: 0.73
+
+    # lying
+    # orn [0, math.pi / 2, x] or [0, 3 * math.pi, x]
+    # height: 0.705
+    # base_orientation = random.randint(0, 2)
+    # if base_orientation == 0:
+        # flat on the ground
+        # if random.randint(0, 1) == 1:
+            # roll = 0
+            # z = 0.71
+        # else:
+            # roll = math.pi
+            # z = 0.73
+        # pitch = 0
+        # z = 0.71
+    # elif base_orientation == 1:
+        # long side on the ground
+        # if random.randint(0, 1) == 0:
+            # roll = math.pi / 2
+            # z = 0.72
+        # else:
+            # roll = 3 * math.pi / 2
+        # pitch = 0
+        # z = 0.72
+    # else:
+        # short side on the ground
+        # roll = 0
+        # if random.randint(0, 1) == 0:
+            # pitch = math.pi / 2
+        # else:
+            # pitch = 3 * math.pi / 2
+        # z = 0.73
+    
+    roll = 0
+    if random.randint(0, 1) == 1:
+        roll = math.pi
+    pitch = 0
+    z = 0.71
+
+    yaw = random.uniform(0, 2 * math.pi) 
+    rotation = p.getQuaternionFromEuler([roll, pitch, yaw])
+
+    # table top goes from x[-1.07, -0.01] y[-0.03, -1.05]
+    border_size = 0.25
+    table_range_x = [-1.07 + border_size, -0.01 - border_size]
+    table_range_y = [-1.05 + border_size, -0.03 - border_size]
+
+    x = random.uniform(*table_range_x) + table_offset_x
+    y = random.uniform(*table_range_y) + table_offset_y
+    position = [x, y, z]
+
+    return {'pos': position, 'orn': rotation}
+
 
 
 def main(args):
@@ -86,7 +141,7 @@ def main(args):
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-o', '--output_dir', type=str, default='./output/',
                         help='the directory in which the resulting images and annotations are saved')
-    parser.add_argument('-n', '--number', type=int, default=10,
+    parser.add_argument('-n', '--number', type=int, default=1,
                         help='the number of images to be generated')
     parser.add_argument('--starting_number', type=int, default=0,
                         help='the number with which image generation starts')
@@ -109,33 +164,33 @@ def main(args):
     paths = [os.path.join(clamp_dir, p) for p in paths]
     marker = np.loadtxt(marker_file, delimiter=',', skiprows=2, usecols=[2, 3, 4])
 
+    env = FlexAssemblyEnv(stepping=False, gui=False)
+    env.remove_camera('global')
+    for clamp_id in env.object_ids['clamps']:
+        p.removeBody(clamp_id)
+
     for j in tqdm.trange(args.starting_number, args.number):
         img_name = '{:05d}'.format(j) + '.png'
         data[img_name] = []
 
-        translation = [0, 0, 0]
-        if random.random() > 0.5:
-            x_rot = math.pi
-        else:
-            x_rot = 0
-        rotation = p.getQuaternionFromEuler([x_rot, 0, random.uniform(0, 2 * math.pi)])
-
-        camera_settings = get_random_camera_settings()
+        model_pose = generate_random_model_pose()
+        camera_settings = get_random_camera_settings(model_pose['pos'])
         # print('Take image with settings:\n', camera_settings)
         # print('  pos:    ', [f'{val:.2f}' for val in camera_settings['pos']])
         # print('  target: ', [f'{val:.2f}' for val in camera_settings['target_pos']])
         # print('  up:     ', [f'{val:.2f}' for val in camera_settings['up']])
 
         # generate the image of the unmarked clamp
-        unmarked = generate_image(get_model_settings(paths[0], translation, rotation), camera_settings)
+        unmarked = generate_image(paths[0], model_pose, camera_settings)
         unmarked = cv.cvtColor(unmarked, cv.COLOR_RGB2BGR)
         # cv.imshow('Unmarked', unmarked)
-        # cv.waitKey(0)
+        # if cv.waitKey(0) == ord('q'):
+            # break
         cv.imwrite(os.path.join(args.output_dir, img_name), unmarked)
 
         for i, path in enumerate(paths[1:]):
             # generate the image of the current marker
-            marker = generate_image(get_model_settings(path, translation, rotation), camera_settings)
+            marker = generate_image(path, model_pose, camera_settings)
 
             # generate the mask
             marker = cv.cvtColor(marker, cv.COLOR_RGB2HSV)
