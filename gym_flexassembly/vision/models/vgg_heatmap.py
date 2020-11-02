0efe954f318197d7efa91473caa7b0567aefa2b6
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2020-11-02 08:43:29 +0100

    vgg heatmap now uses simple upscaling

--- gym_flexassembly/vision/models/vgg_heatmap.py
+++ gym_flexassembly/vision/models/vgg_heatmap.py
@@ -30,28 +30,30 @@ class VGG_Heatmap(torch.nn.Module):
         conv3.add_module('relu3_3', torch.nn.ReLU())
         self.conv3 = conv3
 
-        up3 = torch.nn.Sequential()
-        up3.add_module('up_conv3_2', torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1))
-        up3.add_module('up_relu3_2', torch.nn.ReLU())
-        up3.add_module('up_conv3_1', torch.nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding=1))
-        up3.add_module('up_relu3_1', torch.nn.ReLU())
-        up3.add_module('deconv2', torch.nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1, output_padding=1))
-        self.up3 = up3
-        
-        up2 = torch.nn.Sequential()
-        up2.add_module('up_conv2_2', torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1))
-        up2.add_module('up_relu2_2', torch.nn.ReLU())
-        up2.add_module('up_conv2_1', torch.nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1))
-        up2.add_module('up_relu2_1', torch.nn.ReLU())
-        up2.add_module('deconv1', torch.nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1))
-        self.up2 = up2
+        self.conv_out = torch.nn.Conv2d(in_channels=256, out_channels=point_number, kernel_size=1, stride=1, padding=0)
 
-        up1 = torch.nn.Sequential()
-        up1.add_module('up_conv1_2', torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1))
-        up1.add_module('up_relu1_2', torch.nn.ReLU())
-        up1.add_module('up_conv1_1', torch.nn.Conv2d(in_channels=64, out_channels=point_number, kernel_size=3, stride=1, padding=1))
-        up1.add_module('up_relu1_1', torch.nn.ReLU())
-        self.up1 = up1
+        # up3 = torch.nn.Sequential()
+        # up3.add_module('up_conv3_2', torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1))
+        # up3.add_module('up_relu3_2', torch.nn.ReLU())
+        # up3.add_module('up_conv3_1', torch.nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding=1))
+        # up3.add_module('up_relu3_1', torch.nn.ReLU())
+        # up3.add_module('deconv2', torch.nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1, output_padding=1))
+        # self.up3 = up3
+        
+        # up2 = torch.nn.Sequential()
+        # up2.add_module('up_conv2_2', torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1))
+        # up2.add_module('up_relu2_2', torch.nn.ReLU())
+        # up2.add_module('up_conv2_1', torch.nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1))
+        # up2.add_module('up_relu2_1', torch.nn.ReLU())
+        # up2.add_module('deconv1', torch.nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1))
+        # self.up2 = up2
+
+        # up1 = torch.nn.Sequential()
+        # up1.add_module('up_conv1_2', torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1))
+        # up1.add_module('up_relu1_2', torch.nn.ReLU())
+        # up1.add_module('up_conv1_1', torch.nn.Conv2d(in_channels=64, out_channels=point_number, kernel_size=3, stride=1, padding=1))
+        # up1.add_module('up_relu1_1', torch.nn.ReLU())
+        # self.up1 = up1
 
         if init_vgg:
             self._initialize_weights()
@@ -60,9 +62,11 @@ class VGG_Heatmap(torch.nn.Module):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = self.up3(x)
-        x = self.up2(x)
-        x = self.up1(x)
+        x = self.conv_out(x)
+        x = torch.nn.functional.interpolate(x, scale_factor=4, mode='bilinear', align_corners=False)
+        # x = self.up3(x)
+        # x = self.up2(x)
+        # x = self.up1(x)
         return x
 
     def _initialize_weights(self):
@@ -83,12 +87,8 @@ class VGG_Heatmap(torch.nn.Module):
         self.conv3.conv3_2.bias.data.copy_(pre_train[keys[11]])
         self.conv3.conv3_3.bias.data.copy_(pre_train[keys[13]])
 
-m = VGG_Heatmap(5, False)
-x_0 = torch.rand((1, 3, 224, 224))
-x = m(x_0)
-# x_1 = m.conv1(x_0)
-# x_2 = m.conv2(x_1)
-# x_3 = m.conv3(x_2)
-# x_4 = m.up3(x_3)
-# x_5 = m.up2(x_4)
-# x_6 = m.up1(x_5)
+if __name__ == '__main__':
+    m = VGG_Heatmap(5, False)
+    x_0 = torch.rand((1, 3, 224, 224))
+    x = m(x_0)
+
