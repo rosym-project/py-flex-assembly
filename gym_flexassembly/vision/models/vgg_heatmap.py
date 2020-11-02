157721f3ef208f2858e4499d6dd59e346f5522a1
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2020-11-02 09:12:34 +0100

    add loss computation and timing method to vgg_heatmap model

--- gym_flexassembly/vision/models/vgg_heatmap.py
+++ gym_flexassembly/vision/models/vgg_heatmap.py
@@ -69,6 +69,13 @@ class VGG_Heatmap(torch.nn.Module):
         # x = self.up1(x)
         return x
 
+    def compute_loss(self, labels, predictions):
+        """
+        Loss computation from the re-implementation of the satcked hout
+        glass network in pytorch: TODO: link
+        """
+        return ((labels - predictions) ** 2).mean()
+
     def _initialize_weights(self):
         keys = list(torchvision.models.vgg16(pretrained=True).state_dict())
         self.conv1.conv1_1.weight.data.copy_(pre_train[keys[0]])
@@ -88,7 +95,53 @@ class VGG_Heatmap(torch.nn.Module):
         self.conv3.conv3_3.bias.data.copy_(pre_train[keys[13]])
 
 if __name__ == '__main__':
-    m = VGG_Heatmap(5, False)
-    x_0 = torch.rand((1, 3, 224, 224))
-    x = m(x_0)
+    import argparse
+    import time
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--point_number', type=int, default=10)
+    parser.add_argument('--iterations', type=int, default=100)
+    parser.add_argument('--input_resolution', type=int, default=224)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--device', type=str, default=None,
+                        help='the device on which to perform the computation. \
+                              If this is None cuda:0 is chosen if available, \
+                              else cpu')
+    args = parser.parse_args()
+
+    if args.device:
+        device = torch.device(args.device)
+    else:
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    model = VGG_Heatmap(args.point_number, False)
+    model = model.to(device)
+
+    time_total_forward = 0
+    time_total_backward = 0
+    for i in range(args.iterations):
+        print(f'Iteration {i+1}/{args.iterations}', end='\r')
+        inputs = torch.rand((args.batch_size, 3, args.input_resolution, args.input_resolution),
+                            device=device)
+        outputs = torch.empty((args.batch_size, args.point_number, args.input_resolution, args.input_resolution),
+                              device=device)
+
+        time_start = time.time()
+        predictions = model(inputs)
+        time_forward = time.time()
+
+        loss = model.compute_loss(outputs, predictions)
+        loss.backward()
+        time_backward = time.time()
+
+        time_total_forward += time_forward - time_start
+        time_total_backward += time_backward - time_start
+
+    time_average_forward = (time_total_forward / args.iterations) * 1000
+    time_average_backward = (time_total_backward / args.iterations) * 1000
+
+    print(f'Average time forward {time_average_forward:.1f}ms')
+    print(f'Average time backward {time_average_backward:.1f}ms')
+
+
 
