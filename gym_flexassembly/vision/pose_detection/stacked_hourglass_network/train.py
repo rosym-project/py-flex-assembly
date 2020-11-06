a4764d3ecce4ab4db7b7577b2cc460003fab5991
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2020-11-06 10:51:28 +0100

    reformat train script so that phase can be importet by other modules

--- gym_flexassembly/vision/pose_detection/stacked_hourglass_network/train.py
+++ gym_flexassembly/vision/pose_detection/stacked_hourglass_network/train.py
@@ -14,66 +14,37 @@ from loguru import logger
 from vgg_heatmap import VGGHeatmapComplex, VGGHeatmapSimple
 from point_detection import get_transform, HeatmapDataset
 
-parser = argparse.ArgumentParser(description='train a vgg heatmap model',
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-# dataset params
-parser.add_argument('--dir_train', type=str, required=True,
-                    help='the directory of the training data')
-parser.add_argument('--dir_val', type=str, required=True,
-                    help='the directory of the validation data')
-# model type
-parser.add_argument('--complex', action='store_true',
-                    help='use the more complex vgg heatmap model')
-# training params
-parser.add_argument('--batch_size', type=int, default=8,
-                    help='the batch size used for training/validation')
-parser.add_argument('--epochs', type=int, default=100,
-                    help='number of epochs to train')
-parser.add_argument('--snapshot_epoch', type=int, default=10,
-                    help='every snapshot_epoch the current weights are saved')
-parser.add_argument('--snapshot_dir', type=str, default='./snapshots',
-                    help='snapshots are saved to this directory')
-parser.add_argument('--validation_epoch', type=int, default=1,
-                    help='every validation_epoch the validation loss is computed')
-# continue training
-parser.add_argument('--weights', type=str,
-                    help='load weights to continue training from')
-parser.add_argument('--starting_epoch', type=int, default=0,
-                    help='the epoch at which to start training (should be defined when training is continued)')
-# logging
-parser.add_argument('--logfile', type=str, default='traing.log',
-                    help='the file to which the training progress is logged')
-args = parser.parse_args()
-
-logger.add(args.logfile, level='DEBUG')
-logger.debug(f'Args: {args}')
-
-datasets = { Phase.TRAIN: HeatmapDataset(args.dir_train, get_transform(train=True)),
-             Phase.VALIDATION: HeatmapDataset(args.dir_val, get_transform(train=False)) }
-
-def get_dataloader(phase, dataset):
-    return torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=(phase == Phase.TRAIN))
-dataloaders = dict(map(lambda phase: (phase, get_dataloader(phase, datasets[phase])), datasets))
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-def load_model(complex_type, weights):
-    init_vgg = weights is None
-    point_number = datasets[Phase.TRAIN].point_number
-    if complex_type:
-        model = VGGHeatmapComplex(point_number, init_vgg=init_vgg)
-    else:
-        model = VGGHeatmapSimple(point_number, init_vgg=init_vgg)
-
-    if weights:
-        model.load_state_dict(torch.load(args.weights, map_location=device))
-
-    model = model.float()
-    model = model.to(device)
-    return model
-detector = load_model(args.complex, args.weights)
-optimizer = torch.optim.SGD([{'params': detector.encoder.parameters(), 'lr': 1e-4, 'momentum': 0.95},
-                             {'params': detector.decoder.parameters(), 'lr': 1e-3, 'momentum': 0.90}])
-# lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 100, 0.1)
+def create_parser():
+    parser = argparse.ArgumentParser(description='train a vgg heatmap model',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # dataset params
+    parser.add_argument('--dir_train', type=str, required=True,
+                        help='the directory of the training data')
+    parser.add_argument('--dir_val', type=str, required=True,
+                        help='the directory of the validation data')
+    # model type
+    parser.add_argument('--complex', action='store_true',
+                        help='use the more complex vgg heatmap model')
+    # training params
+    parser.add_argument('--batch_size', type=int, default=8,
+                        help='the batch size used for training/validation')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of epochs to train')
+    parser.add_argument('--snapshot_epoch', type=int, default=10,
+                        help='every snapshot_epoch the current weights are saved')
+    parser.add_argument('--snapshot_dir', type=str, default='./snapshots',
+                        help='snapshots are saved to this directory')
+    parser.add_argument('--validation_epoch', type=int, default=1,
+                        help='every validation_epoch the validation loss is computed')
+    # continue training
+    parser.add_argument('--weights', type=str,
+                        help='load weights to continue training from')
+    parser.add_argument('--starting_epoch', type=int, default=0,
+                        help='the epoch at which to start training (should be defined when training is continued)')
+    # logging
+    parser.add_argument('--logfile', type=str, default='traing.log',
+                        help='the file to which the training progress is logged')
+    return parser
 
 def run_epoch(detector, dataloader, train):
     if not train:
@@ -105,34 +76,71 @@ def _run_epoch(detector, dataloader, train):
             optimizer.step()
     return epoch_loss / len(dataloaders[phase])
 
+def load_model(complex_type, weights, device):
+    init_vgg = weights is None
+    point_number = datasets[Phase.TRAIN].point_number
+    if complex_type:
+        model = VGGHeatmapComplex(point_number, init_vgg=init_vgg)
+    else:
+        model = VGGHeatmapSimple(point_number, init_vgg=init_vgg)
 
-def log_loss(iteration, phase, loss):
-    logger.info(f'Epoch {iteration}/{args.epochs} - {phase.name} - Loss: {loss:.4f}')
+    if weights:
+        model.load_state_dict(torch.load(weights, map_location=device))
+
+    model = model.float()
+    model = model.to(device)
+    return model
+
 
 def save_weights(iteration, detector):
     path = os.path.join(args.snapshot_dir, f'{detector.__class__.__name__}_{i}.model')
     logger.debug(f'Save weights at {path}')
     torch.save(detector.state_dict(), path)
 
-# compute initial training and validation loss
-for phase in Phase:
-    log_loss(0, phase, run_epoch(detector, dataloaders[phase], False))
 
-# perform training
-for i in range(args.starting_epoch + 1, args.epochs + 1):
-    loss = run_epoch(detector, dataloaders[Phase.TRAIN], True)
-    log_loss(i, Phase.TRAIN, loss)
+if __name__ == '__main__':
+    args = parser.parse_args()
+    def log_loss(iteration, phase, loss):
+        logger.info(f'Epoch {iteration}/{args.epochs} - {phase.name} - Loss: {loss:.4f}')
 
-    if i % args.validation_epoch == 0:
-        loss = run_epoch(detector, dataloaders[Phase.VALIDATION], False)
-        log_loss(i, Phase.VALIDATION, loss)
+    logger.add(args.logfile, level='DEBUG')
+    logger.debug(f'Args: {args}')
 
-    if i % args.snapshot_epoch == 0:
-        save_weights(i, detector)
+    datasets = { Phase.TRAIN: HeatmapDataset(args.dir_train, get_transform(train=True)),
+                 Phase.VALIDATION: HeatmapDataset(args.dir_val, get_transform(train=False)) }
 
-    # lr_scheduler.step()
+    def get_dataloader(phase, dataset):
+        return torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=(phase == Phase.TRAIN))
+    dataloaders = dict(map(lambda phase: (phase, get_dataloader(phase, datasets[phase])), datasets))
 
-# if the model was not saved through snapshot dir save it now
-if i % args.snapshot_epoch != 0:
-    save_weights(i, detector)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    detector = load_model(args.complex, args.weights, device)
+    optimizer = torch.optim.SGD([{'params': detector.encoder.parameters(), 'lr': 1e-4, 'momentum': 0.95},
+                                 {'params': detector.decoder.parameters(), 'lr': 1e-3, 'momentum': 0.90}])
+    # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 100, 0.1)
+
+    # compute initial training and validation loss
+    for phase in Phase:
+        log_loss(0, phase, run_epoch(detector, dataloaders[phase], False))
+
+    try:
+        # perform training
+        for i in range(args.starting_epoch + 1, args.epochs + 1):
+            loss = run_epoch(detector, dataloaders[Phase.TRAIN], True)
+            log_loss(i, Phase.TRAIN, loss)
+
+            if i % args.validation_epoch == 0:
+                loss = run_epoch(detector, dataloaders[Phase.VALIDATION], False)
+                log_loss(i, Phase.VALIDATION, loss)
+
+            if i % args.snapshot_epoch == 0:
+                save_weights(i, detector)
+
+            # lr_scheduler.step()
+    except KeyboardInterrupt:
+        logger.trace('Save despite keyboard interruption')
+    finally:
+        # if the model was not saved through snapshot dir save it now
+        if i % args.snapshot_epoch != 0:
+            save_weights(i, detector)
 
