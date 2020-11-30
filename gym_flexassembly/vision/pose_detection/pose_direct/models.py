682888a7612aa0aab4deceb78af98d97224902e4
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2020-11-30 10:55:46 +0100

    fixes for translation model training

--- gym_flexassembly/vision/pose_detection/pose_direct/models.py
+++ gym_flexassembly/vision/pose_detection/pose_direct/models.py
@@ -2,6 +2,28 @@ import torch
 import torchvision
 
 
+class VGG16Backend(torch.nn.Module):
+
+    def __init__(self, pretrained=True):
+        super(VGG16Backend, self).__init__()
+
+        vgg16 = torchvision.models.vgg16(pretrained=pretrained)
+        vgg16_children = list(vgg16.children())
+
+        self.backend_bot = torch.nn.Sequential(*vgg16_children[:-1])
+
+        self.backend_top = vgg16_children[-1]
+        self.backend_top = torch.nn.Sequential(*(list(self.backend_top.children())[:-1]))
+        
+        self.backend_parameters = self.parameters()
+        self.backend_feature_number = 4096
+
+    def forward(self, data):
+        x = self.backend_bot(data)
+        x = x.view(x.shape[0], 25088) # 25088 = 512 * 7 * 7
+        return self.backend_top(x)
+
+
 class Resnet18Base(torch.nn.Module):
 
     def __init__(self, pretrained=True):
@@ -16,10 +38,10 @@ class Resnet18Base(torch.nn.Module):
         self.backend_parameters = self.parameters()
 
     def forward(self, data):
-        return self.backend(data)
+        return self.backend(data).squeeze(dim=3).squeeze(dim=2)
 
 
-class RotationDetector(Resnet18Base):
+class RotationDetector(VGG16Backend):
     """
     A neural network to detect the rotation of a clamp
     """
@@ -32,23 +54,21 @@ class RotationDetector(Resnet18Base):
 
     def forward(self, data):
         features = super().forward(data)
-        features = features.squeeze(dim=3).squeeze(dim=2)
         return self.rotation(features)
 
 
-class TranslationDetector(Resnet18Base):
+class TranslationDetector(VGG16Backend):
     """
     A neural network to detect the rotation of a clamp
     """
 
     def __init__(self, init_backend=True):
-        super(RotationDetector, self).__init__(init_backend)
+        super(TranslationDetector, self).__init__(init_backend)
 
         self.translation = torch.nn.Linear(self.backend_feature_number, 3)
-        self.translation_parameters = self.rotation.parameters()
+        self.translation_parameters = self.translation.parameters()
 
     def forward(self, data):
         features = super().forward(data)
-        features = features.squeeze(dim=3).squeeze(dim=2)
         return self.translation(features)
 
