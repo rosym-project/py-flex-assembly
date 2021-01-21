d959aa26d3cc34a83abf27da972ecd7b7cfaba58
christopher.kreis <ckreis@techfak.uni-bielefeld.de>
2021-01-21 14:56:11 +0100

    code cleanup for feature extraction

--- gym_flexassembly/vision/pose_detection/bounding_box_regression/extract_features.py
+++ gym_flexassembly/vision/pose_detection/bounding_box_regression/extract_features.py
@@ -19,7 +19,7 @@ def main(args):
     print(args)
 
     data = np.loadtxt(args.data_dir + "/data.csv", skiprows=1, usecols=1, delimiter=',', dtype=np.str)
-    features = [["id", "image_name", "bb_center_x", "bb_center_y", "bb_height", "bb_width", "bb_angle"]]
+    features = [["id", "image_name", "bb_center_x", "bb_center_y", "bb_height", "bb_width", "bb_angle", "laplacian_side"]]
 
     for i, f in enumerate(tqdm.tqdm(data)):
         image = cv.imread(args.data_dir + "/" + f)
@@ -67,24 +67,12 @@ def main(args):
 
 
         # ======================================================================
-        # additional features
+        # additional feature: which half has more pixels with laplacian > 0
         # ======================================================================
 
-        # detect hole
-        # doesn't work reliably
-        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        #laplacian = cv.Laplacian(gray, cv.CV_8U)
-        #laplacian = np.where(laplacian > 0, 255, 0).astype(np.uint8)
-        circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 10,
-                               param1=25, param2=15,
-                               minRadius=5, maxRadius=15)
-
-        # gradient detection
+        # compute laplacian
         laplacian = cv.Laplacian(gray, cv.CV_64F)
         laplacian = np.where(laplacian > 0, 1, 0)
-        #cv.imshow("laplacian", np.minimum(laplacian.astype(np.float64) * 255, 255))
-        #if cv.waitKey(0) == ord('q'):
-            #break
 
         # create masks for the two halves of the clamp
         box = cv.boxPoints(bounding_box)
@@ -109,48 +97,37 @@ def main(args):
         mask_2 = np.zeros(image.shape[:2])
         cv.fillPoly(mask_2, [half_2], color=1)
 
+        # count pixels with laplacian > 0
         score_1 = np.sum(np.where(mask_1 == 1, laplacian, 0))
         score_2 = np.sum(np.where(mask_2 == 1, laplacian, 0))
+        laplacian_side = 0 if score_1 > score_2 else 1
 
-        if score_1 > score_2:
-            cv.polylines(image, [half_1], True, color=(0,0,255))
-        else:
-            cv.polylines(image, [half_2], True, color=(0,0,255))
 
-        #cv.polylines(image, [half_1], True, color=(255,0,0))
-        #cv.polylines(image, [half_2], True, color=(0,0,255))
+        # ======================================================================
+        # save feature + visualization
+        # ======================================================================
 
         # append feature vector to list
-        features.append([i, f, bounding_box[0][0], bounding_box[0][1], bounding_box[1][0], bounding_box[1][1], bounding_box[2]])
+        features.append([i, f, bounding_box[0][0], bounding_box[0][1], bounding_box[1][0], bounding_box[1][1], bounding_box[2], laplacian_side])
 
         # visualization
         if args.visualize:
             # draw bounding box
             box = cv.boxPoints(bounding_box)
             box = np.intp(box)
-            #cv.drawContours(image, [box], 0, (0,255,0))
-            cv.drawContours(mask, [box], 0, 125)
+            cv.drawContours(image, [box], 0, (0,255,0))
 
-            """
-            cv.putText(image, '0', tuple(box[0]), 0, 0.4, (0, 0, 0))
-            cv.putText(image, '1', tuple(box[1]), 0, 0.4, (0, 0, 0))
-            cv.putText(image, '2', tuple(box[2]), 0, 0.4, (0, 0, 0))
-            cv.putText(image, '3', tuple(box[3]), 0, 0.4, (0, 0, 0))
-            """
-
-            # draw circles
-            """
-            if circles is not None:
-                circles = np.uint16(np.around(circles))
-                for i in circles[0, :]:
-                    center = (i[0], i[1])
-                    # circle outline
-                    radius = i[2]
-                    cv.circle(image, center, radius, (0, 0, 255), 1)
-            """
+            # mark the half with the stronger laplacian
+            if score_1 > score_2:
+                cv.polylines(image, [half_1], True, color=(0,0,255))
+            else:
+                cv.polylines(image, [half_2], True, color=(0,0,255))
 
             cv.imshow("image", image)
+
+            #cv.drawContours(mask, [box], 0, 125)
             #cv.imshow("mask", mask)
+
             if cv.waitKey(0) == ord('q'):
                 break
 
