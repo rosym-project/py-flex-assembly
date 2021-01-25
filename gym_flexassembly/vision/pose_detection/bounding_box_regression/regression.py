ed3213e65988a7effce23f50da1dfd7d1728de8d
christopher.kreis <ckreis@techfak.uni-bielefeld.de>
2021-01-25 17:20:30 +0100

    working on regression model

--- gym_flexassembly/vision/pose_detection/bounding_box_regression/regression.py
+++ gym_flexassembly/vision/pose_detection/bounding_box_regression/regression.py
@@ -35,7 +35,7 @@ class Regressor():
     def load(self, model_path):
         self.reg = load(model_path)
 
-    def safe(self, model_path):
+    def save(self, model_path):
         dump(self.reg, model_path)
 
     def predict(self, x):
@@ -51,10 +51,10 @@ class Regressor():
         if model_type == Model.SVR:
             if not model_params is None:
                 self.reg = make_pipeline(StandardScaler(), MultiOutputRegressor(
-                    SVR(C=model_params["multioutputregressor__estimator__C"],
+                    SVR(epsilon=0.0005, C=model_params["multioutputregressor__estimator__C"],
                         gamma=model_params["multioutputregressor__estimator__gamma"])))
             else:
-                self.reg = make_pipeline(StandardScaler(), MultiOutputRegressor(SVR()))
+                self.reg = make_pipeline(StandardScaler(), MultiOutputRegressor(SVR(epsilon=0.0005)))
         elif model_type == Model.DECISION_TREE_REGRESSOR:
             if not model_params is None:
                 self.reg = make_pipeline(StandardScaler(),
@@ -64,22 +64,23 @@ class Regressor():
                 self.reg = make_pipeline(StandardScaler(), DecisionTreeRegressor())
         elif model_type == Model.LASSO:
             if not model_params is None:
-                # todo
-                self.reg = make_pipeline(StandardScaler(), MultiTaskLasso())
+                self.reg = make_pipeline(StandardScaler(), MultiTaskLasso(alpha=model_params['multitasklasso__alpha']))
             else:
                 self.reg = make_pipeline(StandardScaler(), MultiTaskLasso())
         elif model_type == Model.ELASTIC_NET:
             if not model_params is None:
-                # todo
-                self.reg = make_pipeline(StandardScaler(), MultiTaskElasticNet())
+                self.reg = make_pipeline(StandardScaler(), MultiTaskElasticNet(
+                        alpha=model_params['multitaskelasticnet__alpha'], l1_ratio=model_params['multitaskelasticnet__l1_ratio']))
             else:
                 self.reg = make_pipeline(StandardScaler(), MultiTaskElasticNet())
 
-    def fit(self, x, y):
+    def fit(self, x, y, n_jobs):
         if self.reg is None:
             print("error: unconfigured model")
             return
-        self.reg.fit(x, y)
+
+        cv_results = cross_validate(self.reg, x, y, n_jobs=n_jobs, return_estimator=True, scoring='neg_mean_squared_error')
+        self.reg = cv_results['estimator'][np.argmax(cv_results['test_score'])]
 
     def train_hyperparameters(self, x, y, n_jobs):
         if self.model_type is None:
@@ -87,20 +88,21 @@ class Regressor():
             return
 
         if self.model_type == Model.SVR:
-            parameters = {'multioutputregressor__estimator__C':[0.0001, 0.001, 0.01, 0.1, 1, 10],
-                        'multioutputregressor__estimator__gamma':[0.0001, 0.001, 0.01, 0.1, 1, 10]}
+            parameters = {'multioutputregressor__estimator__C':[1, 5, 10],
+                        'multioutputregressor__estimator__gamma':[0.001, 0.01, 0.1]}
         elif self.model_type == Model.DECISION_TREE_REGRESSOR:
             parameters = {'decisiontreeregressor__max_depth':[2, 5, 10, 15, 20, 25, None],
                         'decisiontreeregressor__min_samples_split':[2, 5, 10, 15, 20, 25]}
         elif self.model_type == Model.LASSO:
-            pass
+            parameters = {'multitasklasso__alpha':[1e-6, 1e-5, 1e-4, 0.001, 0.01, 0.1]}
         elif self.model_type == Model.ELASTIC_NET:
-            pass
+            parameters = {'multitaskelasticnet__alpha':[1e-6, 1e-5, 1e-4, 0.001, 0.01, 0.1], 'multitaskelasticnet__l1_ratio':[0.1, 0.3, 0.5, 0.7, 0.9]}
 
         grid = GridSearchCV(self.reg, parameters, n_jobs=n_jobs)
         grid.fit(x, y)
         self.hyperparameters = grid.best_params_
-        print(self.hyperparameters)
+
+        print("Best hyperparameters:", self.hyperparameters)
 
         # reconfigure the model using the hyperparameters
         self.config(self.model_type, self.hyperparameters)
@@ -122,15 +124,16 @@ def main(args):
     # load the datasets
     data_train_x = np.loadtxt(args.data_train + "/features.csv", skiprows=1, usecols=[2, 3, 4, 5, 6, 7], delimiter=',')
     data_train_y = np.loadtxt(args.data_train + "/data.csv", skiprows=1, usecols=[2, 3, 4], delimiter=',')
+    #data_train_y = np.loadtxt(args.data_train + "/data.csv", skiprows=1, usecols=[5, 6, 7], delimiter=',')
     data_val_x = np.loadtxt(args.data_val + "/features.csv", skiprows=1, usecols=[2, 3, 4, 5, 6, 7], delimiter=',')
     data_val_y = np.loadtxt(args.data_val + "/data.csv", skiprows=1, usecols=[2, 3, 4], delimiter=',')
-
+    #data_val_y = np.loadtxt(args.data_val + "/data.csv", skiprows=1, usecols=[5, 6, 7], delimiter=',')
 
     # setup and train the model
     reg = Regressor()
-    reg.config(Model.DECISION_TREE_REGRESSOR, None)
+    reg.config(Model.SVR, None)
     reg.train_hyperparameters(data_train_x, data_train_y, args.n_jobs)
-    reg.fit(data_train_x, data_train_y)
+    reg.fit(data_train_x, data_train_y, args.n_jobs)
 
     # evaluate the model
     prediction = reg.predict(data_val_x)
@@ -138,7 +141,7 @@ def main(args):
     print("MAE:", metrics.mean_absolute_error(data_val_y, prediction))
 
     if not args.model_path is None:
-        reg.safe(args.model_path)
+        reg.save(args.model_path)
 
 if __name__ == '__main__':
     main(sys.argv)
