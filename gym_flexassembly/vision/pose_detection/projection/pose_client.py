1153b7dc3481ad5a3a42d43f73635c49962874eb
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2021-03-31 09:56:38 +0200

    pos client uses cosima ros msg and visualizes pose

--- gym_flexassembly/vision/pose_detection/projection/pose_client.py
+++ gym_flexassembly/vision/pose_detection/projection/pose_client.py
@@ -1,11 +1,39 @@
-import rospy
+import matplotlib.pyplot as plt
+import numpy as np
+from pytransform3d import transformations as pt
+from pytransform3d.transform_manager import TransformManager
+from scipy.spatial.transform import Rotation as R
 
-from py_flex_assembly.srv import PoseEstimation
+import rospy
+from cosima_msgs.srv import GetClamp, GetClampResponse
+
+tm = TransformManager()
 
 rospy.wait_for_service('pose_estimation')
 try:
-    estimate = rospy.ServiceProxy('pose_estimation', PoseEstimation)
-    estimation = estimate()
-    print(f'Pose estimation {estimation}')
+    estimate = rospy.ServiceProxy('pose_estimation', GetClamp)
+    estimation = estimate().i_pose
+    pos = estimation.position
+    pos = np.array([pos.x, pos.y, pos.z])
+    _pos = pos * 1000
+    print(f'Pos [{_pos[0]:.2f}, {_pos[1]:.2f}, {_pos[2]:.2f}]')
+
+    orn = estimation.orientation
+    orn = [orn.x, orn.y, orn.z, orn.w]
+    print(f'Orn: {orn}')
+    orn = R.from_quat(orn)
+    _orn = orn.as_euler('zyx', degrees=True)
+    print(f'Orn [{_orn[0]:.2f}, {_orn[2]:.2f}, {_orn[2]:.2f}]')
+
+    orn = estimation.orientation
+    orn = [orn.w, orn.x, orn.y, orn.z]
+    transform = pt.transform_from_pq(np.hstack((pos, orn)))
+    tm.add_transform('clamp', 'world', transform)
+    ax = tm.plot_frames_in('world', s=0.2)
+    ax.set_xlim((-0.75, 0.05))
+    ax.set_ylim((-0.75, 0.05))
+    ax.set_zlim((0.0, 1.0))
+    plt.show()
+
 except rospy.ServiceException as e:
     print(f'Service call failed... {repr(r)}')
