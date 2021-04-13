0e677ec1cdb36f70ae05ada05a21f2d72388fe89
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2021-04-13 10:01:46 +0200

    implement projection from depth/color to color/depht images

--- gym_flexassembly/vision/pose_detection/projection/test_multiple_clamps.py
+++ gym_flexassembly/vision/pose_detection/projection/test_multiple_clamps.py
@@ -89,26 +89,101 @@ tm.add_transform("table3", "world", as_transform(_pos_table, orn_table))
 # exit(0)
 
 
-# Nico <nrabethge@techfak.uni-bielefeld.de>
-# Mara <mbrandt@techfak.uni-bielefeld.de>
-# Franz <franz@techfak.uni-bielefeld.de>
-
 fl = FilterList()
 
 cv.namedWindow('Display', cv.WINDOW_NORMAL)
 cv.resizeWindow('Display', 1600, 900)
 
+def spiral(radius):
+    """
+    Create list to iterate in a spiral outwards.
+    The values are sorted based on the distance to their center.
+    E.g. [0, 1], [1, 0], [0, -1] and [-1, 0] will all appear before [1, 1].
+    """
+    N = 2 * radius - 1
+
+    # Find the unique distances
+    X, Y = np.meshgrid(np.arange(N), np.arange(N))
+    G = np.sqrt(X**2 + Y**2)
+    U = np.unique(G)
+
+    # Identify these coordinates
+    blocks = [[pair for pair in zip(*np.where(G == idx))] for idx in U if idx < N / 2]
+
+    # Permute along the different orthogonal directions
+    directions = np.array([[1,1], [-1,1], [1,-1], [-1,-1]])
+
+    all_R = []
+    for b in blocks:
+        R = set()
+        for item in b:
+            for x in item * directions:
+                R.add(tuple(x))
+        R = np.array(list(R))
+
+        # Sort by angle
+        T = np.array([np.arctan2(*x) for x in R])
+        R = R[np.argsort(T)]
+        all_R.append(R)
+    return np.concatenate(all_R) 
+
+
+def depth_to_color_pixel(pixel, frame_depth, frame_color):
+    intrin_depth = frame_depth.profile.as_video_stream_profile().intrinsics
+    intrin_color = frame_color.profile.as_video_stream_profile().intrinsics
+
+    extrin_depth_to_color = frame_depth.profile.get_extrinsics_to(frame_color.profile)
+    extrin_color_to_depth = frame_color.profile.get_extrinsics_to(frame_depth.profile)
+
+    # Note:
+    # It can happen that the distance value at the exact pixel is invalid (== 0.0)
+    # If this is the case, use a nearby valid pixel as an approximation
+    _pixel = np.array(pixel)
+    for s in spiral(5):
+        depth = frame_depth.get_distance(*(_pixel + s).astype(int))
+        if depth != 0.0:
+            break
+
+    if depth == 0.0:
+        raise RuntimeError('Unkown depth value...')
+
+    pt = rs.rs2_deproject_pixel_to_point(intrin_depth, pixel, depth)
+    pt = rs.rs2_transform_point_to_point(extrin_depth_to_color, pt)
+    pixel_color = rs.rs2_project_point_to_pixel(intrin_color, pt)
+    return pixel_color
+
+def color_to_depth_pixel(pixel, frame_depth, frame_color, depth_scale):
+    intrin_depth = frame_depth.profile.as_video_stream_profile().intrinsics
+    intrin_color = frame_color.profile.as_video_stream_profile().intrinsics
+
+    extrin_depth_to_color = frame_depth.profile.get_extrinsics_to(frame_color.profile)
+    extrin_color_to_depth = frame_color.profile.get_extrinsics_to(frame_depth.profile)
+
+    pixel_depth = rs.rs2_project_color_pixel_to_depth_pixel(
+            frame_depth.data,
+            depth_scale,
+            0.1,
+            1.0,
+            intrin_depth,
+            intrin_color,
+            extrin_depth_to_color,
+            extrin_color_to_depth,
+            pixel)
+    return pixel_depth
+
+depth_scale = pipeline.get_active_profile().get_device().first_depth_sensor().get_depth_scale()
 while True:
     frames = pipeline.wait_for_frames()
     frame_color = frames.get_color_frame()
     frame_depth = frames.get_depth_frame()
-    frame_depth = fl.process(frame_depth)
+    frame_depth = fl.process(frame_depth).as_depth_frame()
 
     intrin = frame_depth.get_profile().as_video_stream_profile().get_intrinsics()
 
     pos_tabl1_in_cam = tm.get_transform('table1', 'cam')[:3, -1]
     pos_tabl2_in_cam = tm.get_transform('table2', 'cam')[:3, -1]
     pos_tabl3_in_cam = tm.get_transform('table3', 'cam')[:3, -1]
+
 
     as_pixel1 = np.array(rs.rs2_project_point_to_pixel(intrin, pos_tabl1_in_cam))
     as_pixel2 = np.array(rs.rs2_project_point_to_pixel(intrin, pos_tabl2_in_cam))
@@ -118,9 +193,7 @@ while True:
     xs = np.array([as_pixel1[0], as_pixel2[0], as_pixel3[0]])
     ys = np.array([as_pixel1[1], as_pixel2[1], as_pixel3[1]])
     zs = [pos_tabl1_in_cam[2], pos_tabl2_in_cam[2], pos_tabl3_in_cam[2]]
-    print('z before', zs)
     zs = np.array(zs) * 1000
-    print('z after', zs)
     inputs = np.array([xs, ys]).T
     from sklearn import linear_model
     model = linear_model.LinearRegression().fit(inputs, zs)
@@ -159,11 +232,17 @@ while True:
     cnts, _ = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     bbs = list(map(lambda cnt: fts.BoundingBox(cv.minAreaRect(cnt)), cnts))
 
+    # TODO: as another filter we could test, if the 3d bounding box matches in length/depth
     def ratio_filter(bb):
-        long_side_length = max(bb.w, bb.h)
-        short_side_length = min(bb.w, bb.h)
-        ratio = long_side_length / short_side_length
-        return 3 < ratio < 6
+        try:
+            long_side_length = max(bb.w, bb.h)
+            short_side_length = min(bb.w, bb.h)
+            ratio = long_side_length / short_side_length
+            return 3 < ratio < 6
+        except ZeroDivisionError:
+            print('Ratio zero?', bb.w, bb.h)
+            print(bb)
+            return False
 
     bbs = list(filter(ratio_filter, bbs))
 
@@ -171,24 +250,37 @@ while True:
     scale_w = img_color.shape[0] / img_depth.shape[0]
 
     img_depth = vis.display_depth_image(img_depth)
-    img_depth = cv.resize(img_depth, img_color.shape[:2][::-1])
-
-    print(scale_h, scale_w)
 
     for bb in bbs:
-        bb.scale(scale_h, scale_w)
-        print(bb.as_int())
-        cv.imshow('test', img_depth[bb.as_slice()])
-        cv.drawContours(img_depth, [bb.as_int()], -1, (0, 255, 0), 3)
+        bb_color = []
+        cv.drawContours(img_depth, [bb.as_int()], -1, (0, 255, 0), 2)
+        for pt in bb.as_points():
+            try:
+                res = depth_to_color_pixel(pt, frame_depth, frame_color)
+                _res = np.array(res).astype(int)
+                bb_color.append(_res)
+                cv.circle(img_color, tuple(_res), 5, (0, 255, 0), -1)
 
-        cv.imshow('Display', np.vstack((img_color, img_depth)))
+                res = color_to_depth_pixel(res, frame_depth, frame_color, depth_scale)
+                _res = np.array(res).astype(int)
+                cv.circle(img_depth, tuple(_res), 3, (0, 0, 255), -1)
+            except RuntimeError as e:
+                print(e)
+                print()
+        bb_color = fts.BoundingBox(cv.minAreaRect(np.array(bb_color)))
+        from gym_flexassembly.vision.pose_detection.projection.side_model import SidePredictor
+        side = fts.detect_side_2(img_color, bb_color, SidePredictor('./models/side_model.pth'))
 
-        print(bb)
-        bb_refined = fts.refine_bb(bb, img_color)
-        cv.drawContours(img_color, [bb_refined.as_int()], -1, (0, 255, 0), 3)
+    img_depth = cv.resize(img_depth, img_color.shape[:2][::-1])
+    cv.imshow('Display', np.vstack((img_color, img_depth)))
 
-        cv.imshow('Display', np.vstack((img_color, img_depth)))
 
-        if cv.waitKey(0) == ord('q'):
-            break
+    print()
+    # print(bb)
+    # bb_refined = fts.refine_bb(bb, img_color)
+    # cv.drawContours(img_color, [bb_refined.as_int()], -1, (0, 255, 0), 3)
+    key = cv.waitKey(0)
+    if key == ord('q'):
+        print('Break..')
+        exit(0)
 
